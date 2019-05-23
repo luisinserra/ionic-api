0589c25cd9eb5e3aa5a0558678972e0da14c5024
Luis Inserra <luisinserra@yahoo.com.br>
2019-05-23 20:25:53 -0300

    salvando alterações...

--- src/pages/login/login.ts
+++ src/pages/login/login.ts
@@ -10,13 +10,19 @@ export class LoginPage {
 
     private alertCtrl;
     public login: string = 'aa';
+    tSenha: string;
 
   constructor(public navCtrl: NavController, alertCtrl : AlertController) {
     this.alertCtrl = alertCtrl;
   }
 
+  handleChange = name => event => {
+    let valor=event.target.value;
+    console.log("name: "+name+", valor: "+valor);
+};
+
   clicou = () => {
-      let msg="Botão pressionado! para "+this.login;
+      let msg="Botão pressionado! para "+this.tSenha;
       const alert = this.alertCtrl.create({
         title: 'Vamos nessa!',
         subTitle: msg,
